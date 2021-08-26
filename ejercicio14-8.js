ace785ee619619314f39760e48175d945cebc6cb
Nicolas Davila <nicolasdavila12@gmail.com>
2021-08-26 16:35:42 -0300

    ejercicio21deAgosto

---
+++ ejercicio14-8.js
@@ -0,0 +1,20 @@
+//
+//
+//
+//
+//
+//
+
+console.log(document.body);
+
+let dato = document.getElementsByTagName("li")
+
+console.log(dato)
+
+let infoCompleta = document.querySelectorAll("li")
+console.log (infoCompleta)
+
+for (let i = 0; i < infoCompleta.length; i++) {
+    console.log(infoCompleta[i])
+
+}
